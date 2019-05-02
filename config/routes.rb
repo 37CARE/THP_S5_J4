edc351fa6cfad2d81232925cbf42b607f000279a
37CARE <mhamed@37care.co>
2019-05-02 22:41:42 +0200

    création dans un nouveau repo

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,11 @@ Rails.application.routes.draw do
   resources :users
   resources :cities
   resources :sessions, only: [:new, :create, :destroy]
-
+  resources :likes, only: [:show, :new, :create, :destroy]
 
   get '/team', to: 'static_pages#team'
   get '/contact', to: 'static_pages#contact'
   get '/welcome/', to: 'static_pages#welcome'
   get '/welcome/:anything', to: 'static_pages#welcome'
-  get 'sessions/new'
 
 end
